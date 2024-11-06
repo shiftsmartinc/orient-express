4b57fe236b2314b3a90fc398e6baa7fb9a1c75e7
Alexey Zankevich <alex.zankevich@shiftsmart.com>
2024-11-06 17:34:41 -0500

    Fixed package & imports

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="orient_express",
-    version="0.1.2",
+    version="0.2.1",
     description="A single-module library for [describe functionality briefly]",
     author="Alex Zankevich",
     author_email="alex.zankevich@shiftsmart.com",
     url="https://github.com/shiftsmartinc/orient-express",  # Replace with your repository URL
     packages=find_packages(),  # Automatically finds all packages in the project
-    install_requires=["google-cloud-aiplatform", "google-cloud-storage"],
+    install_requires=["google-cloud-aiplatform", "google-cloud-storage", "pandas"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # Replace with the appropriate license
