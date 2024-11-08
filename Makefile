8ccc744a43f56e67aaf7fea10f794d71aa7c5ce8
Alexey Zankevich <alex.zankevich@shiftsmart.com>
2024-11-08 13:07:48 -0500

    Updated

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build:
-	docker buildx build -t orient-express:latest  -f docker-xgboost-scikit-learn/Dockerfile .
+	docker buildx build --tag us-west1-docker.pkg.dev/shiftsmart-api/orient-express-inference-xgboost-scikit-learn:latest -f docker-xgboost-scikit-learn/Dockerfile .
--- docker-xgboost-scikit-learn/run_inference_server.py
+++ docker-xgboost-scikit-learn/run_inference_server.py
@@ -14,7 +14,7 @@ class ScikitLearnPipelineModel(Model):
         self.model = None
 
     def load(self):
-        model_path = os.path.join(self.artifacts_path, "model.joblib")
+        model_path = f"{self.artifacts_path}/model.joblib"
         logging.info(f"Loading model from {model_path}")
 
         # Create a GCSFileSystem object
@@ -24,17 +24,23 @@ class ScikitLearnPipelineModel(Model):
         with fs.open(model_path, "rb") as f:
             self.model = joblib.load(f)
 
+        self.ready = True
         logging.info("Model loaded successfully")
         return self
 
-    def predict(self, inputs):
-        logging.info(inputs)
-        print(inputs)
-        return self.model.predict(inputs)
+    def predict(self, inputs, *args, **kwargs):
+        logging.info(f"Predicting {len(inputs)} items")
+
+        input_df = pd.DataFrame(inputs)
+
+        predictions = self.model.predict(input_df)
+        response = {"predictions": predictions.tolist()}
+        return response
 
 
 if __name__ == "__main__":
     logging.config.fileConfig("logging.conf")
+
     try:
         storage_uri = os.environ["AIP_STORAGE_URI"]
 
@@ -42,9 +48,7 @@ if __name__ == "__main__":
         model.load()
 
         # Start the model server with the loaded model
-        model_server = ModelServer(
-            http_port=8080, workers=1, configure_logging=True, log_config="logging.conf"
-        )
+        model_server = ModelServer(http_port=8080, workers=1)
         model_server.start([model])
     except Exception as e:
         logging.exception("Failed to start model")
