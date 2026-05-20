346f6580e393784dd28cd506cf2242a5da0f0f59
Ian <igm503@gmail.com>
2026-05-20 19:16:48 -0400

    valid mask

--- orient_express/predictors/semantic_segmentation.py
+++ orient_express/predictors/semantic_segmentation.py
@@ -7,17 +7,19 @@ import torch
 import torch.nn.functional as F
 
 from .predictor import OnnxSessionWrapper, ImagePredictor
-from ..utils.image_processor import pil_to_opencv, opencv_to_pil
+from ..utils.image_processor import mask_to_base64, pil_to_opencv, opencv_to_pil
 
 
 @dataclass
 class SemanticSegmentationPrediction:
     class_mask: np.ndarray
+    valid_mask: np.ndarray
     conf_masks: np.ndarray
 
     def to_dict(self, include_conf_masks: bool = False):
         dict_repr = {
-            "class_mask": self.class_mask.tolist(),
+            "class_mask": mask_to_base64(self.class_mask.astype(np.uint8)),
+            "valid_mask": mask_to_base64(self.valid_mask.astype(np.uint8)),
         }
         if include_conf_masks:
             dict_repr["conf_masks"] = self.conf_masks.tolist()
@@ -67,12 +69,12 @@ class SemanticSegmentationPredictor(ImagePredictor):
         raw_outputs = self.model(images)
         outputs = []
         for masks in raw_outputs:
-            class_mask = np.argmax(masks, axis=0)
-            max_probs = np.max(masks, axis=0)
-            class_mask = np.where(max_probs >= confidence, class_mask, -1)
+            class_mask = np.argmax(masks, axis=0).astype(np.uint8)
+            valid_mask = np.max(masks, axis=0) >= confidence
             outputs.append(
                 SemanticSegmentationPrediction(
                     class_mask=class_mask,
+                    valid_mask=valid_mask,
                     conf_masks=masks,
                 )
             )
@@ -89,7 +91,7 @@ class SemanticSegmentationPredictor(ImagePredictor):
 
         for class_id, class_name in self.classes.items():
             fill_color = self.color_scheme.get(class_name, (120, 120, 120))
-            mask_overlay[prediction.class_mask == class_id] = fill_color[:3]
+            mask_overlay[(prediction.class_mask == class_id) & prediction.valid_mask] = fill_color[:3]
 
         annotated_image = cv2.addWeighted(
             mask_overlay, mask_opacity, opencv_image, 1 - mask_opacity, 0
